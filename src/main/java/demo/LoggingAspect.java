8799c091660f15a07a953addbb189929626fd437
dollytiwari <dollytiwari393@gmail.com>
2022-09-18 23:36:29 +0530

    AOP @after

--- src/main/java/demo/LoggingAspect.java
+++ src/main/java/demo/LoggingAspect.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
     @Before("execution(* demo.ShoppingCart.checkout())")
-    public void logger(){
-        System.out.println("logger");
+    public void beforeLogger(){
+        System.out.println("before logger");
+    }
+
+    @After("execution(* demo.ShoppingCart.checkout())")
+    public void afterLogger(){
+        System.out.println("after logger");
     }
 }
